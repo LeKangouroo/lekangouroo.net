267a7c9a7a4db37495a92314036f006bb5de6190
LeKangouroo <contact@lekangouroo.me>
2020-08-16 19:07:11 +0200

    set document.title on route update

--- src/js/core/router.js
+++ src/js/core/router.js
@@ -1,14 +1,21 @@
 import { createRouter } from "modules/network/routing";
 import events from "core/events";
 
+const getTitle = sectionName => "@@ENV" === "PRODUCTION"
+  ? `${sectionName} - ${window.location.hostname}`
+  : `[@@ENV] ${sectionName} - ${window.location.hostname}`;
+
 const routes = [
-  { name: "home", uri: "/home" }
+  { name: "home", uri: "/home", data: { title: getTitle("Accueil") } }
 ];
 
 const router = createRouter(window, routes);
 
 router.setDefaultRoute("home");
+router.onRouteChange(route => {
 
-router.onRouteChange((route) => events.notifyObservers("router:update", route));
+  document.title = route.data.title;
+  events.notifyObservers("router:update", route);
+});
 
 export default router;
